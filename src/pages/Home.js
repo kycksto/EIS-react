8fdb7232f18d00f1b05b965c63b92405f5f814d4
Adam gif <kycksto03@gmail.com>
2022-09-02 17:51:11 +0700

    build

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls, Stars } from '@react-three/drei'
 
 import { Effects } from '../components/main/Effects'
-import Voice from '../components/main/Voice'
 import EnvLight from '../components/main/EnvLight'
 import CubeWhole from '../components/main/CubeWhole'
 import { useStateContext } from '../contexts/ContextProvider'
@@ -11,7 +10,6 @@ function Home() {
   const { currentMode } = useStateContext();
   return (
     <div className='position: fixed w-full h-full'>
-      <Voice/>
       <Canvas gl={{ logarithmicDepthBuffer: true, antialias: true }} dpr={[1, 1.5]} camera={{ position: [9, -3, 9], fov: 50 }}>
       <color attach="background" args={['black']} />
         <CubeWhole/>
